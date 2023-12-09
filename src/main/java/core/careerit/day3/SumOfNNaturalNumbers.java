6a8fec47b82fe03567e09ab2ffceb6e1754788fe
vkmaddineni <vkmaddineni@gmail.com>
2023-12-09 17:47:16 -0600

    Added SumOfNNaturalNumbers program

---
+++ src/main/java/core/careerit/day3/SumOfNNaturalNumbers.java
@@ -0,0 +1,15 @@
+package core.careerit.day3;
+
+import java.util.Scanner;
+
+public class SumOfNNaturalNumbers {
+    public static void main(String[] args){
+        // Sum of N Natural Numbers
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of number : ");
+        int num = sc.nextInt();
+        int sum = (num * (num+1))/2;
+        System.out.println("Sum of the first "+num+" natural numbers is : "+sum);
+
+    }
+}
